8af25207a965668791eca96df46e661c21d776e9
Ethan Morissette <ethan.morissette@Ethans-MacBook-Pro-2.local>
2022-09-09 07:44:35 -0700

    Making adjustments to php

--- web/modules/form_task/src/Form/FormTask.php
+++ web/modules/form_task/src/Form/FormTask.php
@@ -5,48 +5,47 @@ namespace Drupal\form_task\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
+/**
+ * Implements a example task form using FORM API.
+ */
 class FormTask extends FormBase {
-    
-    /* 
-        Get our form
-    */
-    public function getFormId()
-    {
-        return 'form_task';
-    }
 
-    /* 
-        Create our form
-    */
-    public function buildForm(array $form, FormStateInterface $form_state)
-    {
-        $form['task_one'] = array(
-            '#type' => 'textfield',
-            '#title' => t('Enter task:'),
-            '#required' => TRUE,
-        );
-        return $form;
-    }
 
-    /* 
-        Check our form
-    */
-    public function validateForm(array &$form, FormStateInterface $form_state)
-    {
-        if(strlen($form_state->getValue('task_one')) < 2) {
-            $form_state->setErrorByName('task_one', $this->t('Please enter a valid task'));
-        }
-    }
+  public function getFormId() {
+    return 'form_task';
+  }
 
-    /*
-        Submnit the form and it's data
-    */
-    public function submitForm(array &$form, FormStateInterface $form_state)
-    {
-        \Drupal::messenger()->addMessage(t("Task list submitted!"));
-            foreach ($form_state->getValues() as $key => $value) {
-                \Drupal::messenger()->addMessage($key . ': ' . $value);
-            }
+  /**
+   * Creates form using form api
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['task_one'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Your task'),
+    ];
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+      '#button_type' => 'primary',
+    ];
+    return $form;
+  }
+
+  /**
+   * Validates the length of the task entered
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (strlen($form_state->getValue('task_one')) < 1) {
+      $form_state->setErrorByName('task_one', $this->t('The task entered is too short, enter a longer one!'));
     }
+  }
+
+  /**
+   * Submits form and displays entered task
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->messenger()->addStatus($this->t('Your task is @task', ['@task' => $form_state->getValue('task_one')]));
+  }
 
 }
